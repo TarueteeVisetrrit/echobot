5be3da163aabfbe09c9c3903a54b201d7a5c8127
taruetee <tarueteevi@outlook.com>
2018-03-13 19:51:11 +0700

    try new code

--- index.js
+++ index.js
@@ -3,20 +3,19 @@
 const express = require("express");
 var mysql = require('mysql');
 const bodyParser = require("body-parser");
-
 const restService = express();
 
 
 
 //create connection Code doesn't works
-// var db = mysql.createConnection({
-//  host     : 'us-cdbr-iron-east-05.cleardb.net',
-//  user     : 'bc72622898452a',
-//  password : '3244bd28',
-//  database: 'heroku_8d64b4cb3978d80'
-// });
+var db_config = {
+ host     : 'us-cdbr-iron-east-05.cleardb.net',
+ user     : 'bc72622898452a',
+ password : '3244bd28',
+ database: 'heroku_8d64b4cb3978d80'
+};
 
-//db.connect();
+var connection;
 
 //connect to db Code doesn't works
 // db.connect((err) =>{
@@ -25,6 +24,30 @@ const restService = express();
 //   }
 //   console.log('Mysql connected.....');
 // }); 
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since
+													// the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
+
 restService.use(
   bodyParser.urlencoded({
     extended: true
