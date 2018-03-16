095c7b00ec3a8a8e4a6be679b9f1fc86b6764b9e
taruetee <tarueteevi@outlook.com>
2018-03-16 10:20:05 +0700

    add "/bot"

--- index.js
+++ index.js
@@ -55,6 +55,7 @@ restService.use(
 );
 restService.use(bodyParser.json());
 
+
 restService.get("/hello",function(req,res){
  connection.query('SELECT * from trainee', function(err, rows, fields) {
         if (err) {
@@ -66,6 +67,28 @@ restService.get("/hello",function(req,res){
 //	res.send('Hello world');
 });
 
+restService.post("/bot",function(req,res)){
+  var input = req. body.result.parameter;
+  var speech = " ";
+  switch(input){
+    case "Timetable":
+      speech = "Timetable case";
+      break;
+    case "My progress":
+      speech = "My progress case";
+      break;
+    case "My task":
+      speech = "My task case";
+      break;
+    default:
+      speech = "Type again";
+  } 
+  return res.json({
+    speech: speech,
+    displayText: speech,
+    source: "webhook-echo-sample"
+  });
+}
 restService.post("/echo", function(req, res) {
   var speech =
     req.body.result &&
