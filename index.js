77f7507e59f6e7e06bd71248f0ec29360134686a
taruetee <tarueteevi@outlook.com>
2018-03-24 11:22:41 +0700

    try 2

--- index.js
+++ index.js
@@ -74,10 +74,10 @@ restService.post("/bot", function(req,res){
 
   if(input =="scheduleResult"){
   	var input1 = req.body.result.parameters.dayOfWeek;
-  	fetchClassSchedule(input1,function(result){
-  		speech = result;
+  	fetchClassSchedule(input1,function(rows){
+  		speech = rows;
   		console.log("result print");
-  		speech = "Hahaha"; 
+  		//speech = "Hahaha"; 
   	})
 
   // 	var sql = "SELECT course_name, time_start, time_finish FROM class INNER JOIN timetable ON class.course_id = timetable.course_id WHERE timetable.course_day = ? ORDER by time_start";
