9fbcbc74d388327e715fcd845296bc8201a4c6cd
taruetee <tarueteevi@outlook.com>
2018-04-04 20:19:38 +0700

    edit output

--- index.js
+++ index.js
@@ -118,8 +118,15 @@ restService.post("/bot", function(req,res){
   	var progressInput =  req.body.result.parameters.ProgressInput;
     var sql = "SELECT `trainee`.`Firstname`,`trainee`.`Lastname` ,`class`.`course_name`, `testresult`.`score`,`testresult`.`result`,`testresult`.`date`,`testresult`.`comment` FROM (`trainee` INNER JOIN `testresult` ON `trainee`.`studentID` = `testresult`.`studentID`) INNER JOIN `class` ON `testresult`.`course_id` = `class`.`course_id` WHERE `trainee`.`Firstname` = ? AND `trainee`.`Lastname`= ? AND `class`.`course_name`=  ? ";
   	if(progressInput != null ){
-  		fetchProgress([name,surname,course],function(result){
-  			speech = result; 
+  		fetchProgress([name,surname,course],function(rows){
+  			console.log("blablabla");
+				speech = rows;
+	  			console.log("result: " +speech);
+	  			return res.json({
+	  				speech: speech,
+    				displayText: speech,
+    				source: "webhook-echo-sample"
+  				}) 
   		})
   //     connection.query(sql,[name,surname,course], function (err,rows,fields){
   //     	if (err) {
@@ -137,18 +144,25 @@ restService.post("/bot", function(req,res){
   		//var speech1  = "my training progress"; 
   	}else{
   		speech = "Type again with this format: Progress, Myname, MyLastname, Basic combat pistol.";
-  	}
-  	return res.json({
+  		return res.json({
     	speech: speech,
     	displayText: speech,
     	source: "webhook-echo-sample"
   	});
+  	}
+  	
   }else if(input == "TrainingTask.TrainingTask-custom"){
   	var name = req.body.result.parameters.Firstname; 
   	var surname = req.body.result.parameters.Lastname; 
   	var course = req.body.result.parameters.Courses;
   	fetchTask([name,surname,course],function(result){
   		speech = result;
+  		return res.json({
+    	speech: speech,
+    	displayText: speech,
+    	source: "webhook-echo-sample"
+  	});
+
   	})
     // var sql1 = "SELECT `trainee`.`studentID`,`enroll`.`P_id`,`enroll`.`Day_no`,`course_name`,`t_description` FROM ((`enroll` INNER JOIN `trainee` ON `enroll`.`studentID`= `trainee`.`StudentID` ) INNER JOIN `class` ON `enroll`.`course_id` = `class`.`course_id`) INNER JOIN (`tasktoday` INNER JOIN `curriculum` ON `tasktoday`.`t_id`=`curriculum`.`t_id`) ON `enroll`.`P_id` = `tasktoday`.`P_id` AND `enroll`.`Day_no`= `tasktoday`.`Day_no` AND `class`.`Level`= `tasktoday`.`Level`WHERE `trainee`.`FirstName`=? AND `trainee`.`LastName`=? AND `class`.`course_name`=?";
     // connection.query(sql1,[name,surname,course],function(err,rows,fields) {
@@ -164,12 +178,12 @@ restService.post("/bot", function(req,res){
         
     // });
     // connection.end();
-  	speech = "My task case for "+name+" "+surname+" on course: "+course;
-  	return res.json({
-    	speech: speech,
-    	displayText: speech,
-    	source: "webhook-echo-sample"
-  	});
+  	// speech = "My task case for "+name+" "+surname+" on course: "+course;
+  	// return res.json({
+   //  	speech: speech,
+   //  	displayText: speech,
+   //  	source: "webhook-echo-sample"
+  	// });
   }else if(input == "Packageoptions.Packageoptions-custom.Packageoptions-private-price"){
   	var numTraining = req.body.result.parameters.number;
   	var courseType  = req.body.result.parameters.Courses;
@@ -210,14 +224,19 @@ restService.post("/bot", function(req,res){
   	var courseInput = req.body.result.parameters.Courses; 
   	fetchSpecifyTimetable(courseInput,function(result){
   			speech = result; 
+  			return res.json({
+		    	speech: speech,
+		    	displayText: speech,
+		    	source: "webhook-echo-sample"
+		  	});
   	})
-  	speech = "Schedule of "+courseInput; 
+  	// speech = "Schedule of "+courseInput; 
 
-  	return res.json({
-    	speech: speech,
-    	displayText: speech,
-    	source: "webhook-echo-sample"
-  	});
+  	// return res.json({
+   //  	speech: speech,
+   //  	displayText: speech,
+   //  	source: "webhook-echo-sample"
+  	// });
 
   }else if(input == "Check-in.Check-in-custom"){
   	var name = req.body.result.parameters.Firstname; 
@@ -309,8 +328,10 @@ function fetchTask([name,surname,course],callback){
         	speech1 = speech1+"\n"+rows[i].t_description;	
         }
         console.log(speech1);
+        console.log(speech1);
+        callback(speech1);
+        return;
     });
-    return callback(speech1);
     connection.end();
 }
 
@@ -326,8 +347,11 @@ function fetchProgress([name,surname,course],callback){
       		speech1 = rows[i].Firstname+" "+rows[i].Lastname+" "+rows[i].result+" "+rows[i].course_name+" with score of "+rows[i].score+" out of 100 (pass score is 85). Test on "+rows[i].date+"\n"+"Comment: "+rows[i].comment+"\n"+"Course that you may interest: "+rows[i].Recommended_course; 	 
       	}
       	console.log(speech1);
+      	console.log(speech1);
+        callback(speech1);
+        return;
 	});
-  	return callback(speech1); 
+  	
   	connection.end();
 }
 
@@ -382,8 +406,11 @@ function fetchSpecifyTimetable(courseInput,callback){
         	//speech1 = "wow";
         }
         console.log(speech1);
+        console.log(speech1);
+        callback(speech1);
+        return;
     });
-    return callback(speech1);
+    
     connection.end();
 }
 
